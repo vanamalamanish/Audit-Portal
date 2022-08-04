2989c9acdfd988944c002e190e166eec021388a5
Vanamala Manish <vanamalamanish4@gmail.com>
2022-08-04 11:27:03 +0530

    added service and login component

---
+++ src/app/UserCredentials.ts
@@ -0,0 +1,8 @@
+export class UserCredentials{
+    userName!:String;
+    password!:String;
+    constructor(userName:String,password:String){
+    this.userName=userName;
+    this.password=password;
+    }
+}
